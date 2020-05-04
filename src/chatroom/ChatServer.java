3d90e3f9eba96952eb079ae93618bfda20fd2523
Kayleigh Calder <59836279+calderkayleigh@users.noreply.github.com>
2020-05-03 21:04:26 -0400

    Update ChatServer.java

--- src/chatroom/ChatServer.java
+++ src/chatroom/ChatServer.java
@@ -7,76 +7,119 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
- * YOUR NAME HERE
+ * Kayleigh Calder
  */
-public class ChatServer extends ChatWindow {
+public class ChatServer extends ChatWindow
+{
 
-	private ClientHandler handler;
+	private ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+	public static String chatServerName = "Chat Server";
+	public static int portNumber = 2113;
 
 	public ChatServer(){
 		super();
-		this.setTitle("Chat Server");
+		this.setTitle(chatServerName);
 		this.setLocation(80,80);
 
 		try {
 			// Create a listening service for connections
 			// at the designated port number.
-			ServerSocket srv = new ServerSocket(2113);
+			ServerSocket srv = new ServerSocket(portNumber);
 
-			while (true) {
+			while (true)
+			{
 				// The method accept() blocks until a client connects.
 				printMsg("Waiting for a connection");
 				Socket socket = srv.accept();
 
-				handler = new ClientHandler(socket);
-				handler.handleConnection();
+				if(socket != null)
+				{
+					ClientHandler handler = new ClientHandler(socket);
+					Thread newThread = new Thread(handler);
+					newThread.start();
+					clientHandlers.add(handler);
+				}
 			}
 
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			System.out.println(e);
 		}
 	}
 
 	/** This innter class handles communication to/from one client. */
-	class ClientHandler {
+	class ClientHandler implements Runnable{
 		private PrintWriter writer;
 		private BufferedReader reader;
+		private String username;
 
-		public ClientHandler(Socket socket) {
-			try {
+		public ClientHandler(Socket socket)
+		{
+			try
+			{
 				InetAddress serverIP = socket.getInetAddress();
 				printMsg("Connection made to " + serverIP);
 				writer = new PrintWriter(socket.getOutputStream(), true);
 				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				username = reader.readLine();
+				printMsg(username + "has joined");
+				sendMsg("Hi " + username + " You have joined the classroom");
 			}
-			catch (IOException e){
+			catch (IOException e)
+			{
 					printMsg("\nERROR:" + e.getLocalizedMessage() + "\n");
-				}
+			}
 		}
-		public void handleConnection() {
-			try {
-				while(true) {
+		public void handleConnection()
+		{
+			System.out.println("handle Connection");
+			try
+			{
+				while(true)
+				{
+					System.out.println("handle connection");
 					// read a message from the client
-					readMsg();
+					sendToAllInRoom(readMsg());
 				}
 			}
-			catch (IOException e){
+			catch (IOException e)
+			{
 				printMsg("\nERROR:" + e.getLocalizedMessage() + "\n");
 			}
 		}
 
-		/** Receive and display a message */
-		public void readMsg() throws IOException {
+		/** Receive and display a message
+		 * @return*/
+		public String readMsg() throws IOException
+		{
 			String s = reader.readLine();
 			printMsg(s);
+			return s;
 		}
+
 		/** Send a string */
-		public void sendMsg(String s){
+		public void sendMsg(String s)
+		{
 			writer.println(s);
 		}
 
+		//send the message to all users
+		public void sendToAllInRoom (String s)
+		{
+			//iterate through clients and send the message
+			for(int i=0; i<clientHandlers.size(); i++)
+			{
+				clientHandlers.get(i).sendMsg(s);
+			}
+		}
+		public void run()
+		{
+			handleConnection();
+		}
 	}
 
 	public static void main(String args[]){
