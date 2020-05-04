3fc1f9492e598107a1f62e9b2d8fe4906e6d9d54
Kayleigh Calder <59836279+calderkayleigh@users.noreply.github.com>
2020-05-03 21:05:22 -0400

    Update ChatClient.java

--- src/chatroom/ChatClient.java
+++ src/chatroom/ChatClient.java
@@ -10,15 +10,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Random;
 
 /**
- * YOUR NAME HERE
+ * Kayleigh Calder
  */
-public class ChatClient extends ChatWindow {
-
-	// Inner class used for networking
-	private Communicator comm;
-
+public class ChatClient extends ChatWindow
+{
 	// GUI Objects
 	private JTextField serverTxt;
 	private JTextField nameTxt;
@@ -26,16 +24,26 @@ public class ChatClient extends ChatWindow {
 	private JTextField messageTxt;
 	private JButton sendB;
 
-	public ChatClient(){
+	//variable declarations
+	private String username;
+	public String chatClientName = "Chat Client";
+	private String chatRoom = "Default";
+
+	public ChatClient()
+	{
 		super();
-		this.setTitle("Chat Client");
+		this.setTitle(chatClientName);
 		printMsg("Chat Client Started.");
+
+		//assign client a username
+		Random rand = new Random();
+		username = "UnknownUser" + rand.nextInt(1000);
 
 		// GUI elements at top of window
 		// Need a Panel to store several buttons/text fields
 		serverTxt = new JTextField("localhost");
 		serverTxt.setColumns(15);
-		nameTxt = new JTextField("Name");
+		nameTxt = new JTextField(username);
 		nameTxt.setColumns(10);
 		connectB = new JButton("Connect");
 		JPanel topPanel = new JPanel();
@@ -64,32 +72,40 @@ public class ChatClient extends ChatWindow {
 	}
 
 	/** This inner class handles communication with the server. */
-	class Communicator implements ActionListener{
+	class Communicator implements ActionListener, Runnable
+	{
 		private Socket socket;
 		private PrintWriter writer;
 		private BufferedReader reader;
-		private int port = 2113;
 
 		@Override
 		public void actionPerformed(ActionEvent actionEvent) {
-			if(actionEvent.getActionCommand().compareTo("Connect") == 0) {
+			if(actionEvent.getActionCommand().compareTo("Connect") == 0)
+			{
 				connect();
 			}
 			else if(actionEvent.getActionCommand().compareTo("Send") == 0) {
-				sendMsg(messageTxt.getText());
+				sendMsg();
 			}
 		}
 
 		/** Connect to the remote server and setup input/output streams. */
 		public void connect(){
 			try {
-				socket = new Socket(serverTxt.getText(), port);
+				socket = new Socket(serverTxt.getText(), ChatServer.portNumber);
 				InetAddress serverIP = socket.getInetAddress();
 				printMsg("Connection made to " + serverIP);
 				writer = new PrintWriter(socket.getOutputStream(), true);
 				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-				sendMsg("Hello server");
+				//print the username
+				username = nameTxt.getText();
+				writer.println(username);
+				readMsg();
+
+				//create a new thread
+				Thread newThread = new Thread(this);
+				newThread.start();
 
 			}
 			catch(IOException e) {
@@ -101,10 +117,92 @@ public class ChatClient extends ChatWindow {
 			String s = reader.readLine();
 			printMsg(s);
 		}
+
 		/** Send a string */
-		public void sendMsg(String s){
-			writer.println(s);
+		public void sendMsg()
+		{
+			//declare variables
+			String message;
+
+			//if text is blank, do next send the message
+			if(messageTxt.getText().length() == 0)
+			{
+				return;
+			}
+
+			/**Check to see if the username has been updated*/
+
+			char[] messageArray = messageTxt.getText().toCharArray();
+			char[] updateUsernameMessage = "/name".toCharArray();
+			boolean updateUsername = true;
+
+			//cases where the username is not updated
+			for(int i = 0; i<messageArray.length; i++)
+			{
+				if(updateUsernameMessage.length> messageArray.length || (i<updateUsernameMessage.length && messageArray[i] != updateUsernameMessage[i]))
+				{
+					updateUsername = false;
+				}
+
+			}
+
+			//send message and update username
+			if(updateUsername)
+			{
+				//placeholder for the new username
+				String updatedUsername = "";
+
+				//iterate through the message
+				for(int i = updateUsernameMessage.length; i< messageArray.length; i++)
+				{
+					if(messageArray[i] != ' ')
+					{
+						updatedUsername += messageArray[i];
+					}
+				}
+				//change the username if a new username is provided
+				if(updatedUsername.length() != 0)
+				{
+					//confirmation message
+					message = username + " is changing their name to " + updatedUsername;
+					//write confirmation message
+					writer.println(message);
+					//update the username for future messages
+					username = updatedUsername;
+				}
+				else
+				{
+					printMsg("Error");
+				}
+			}
+
+			//send message without updating username
+			else
+			{
+				//text sent
+				message = "[" +chatRoom +"]" + username + " " +messageTxt.getText();
+				//write the text
+				writer.println(message);
+			}
+			messageTxt.setText(null);
+
 		}
+		@Override
+		public void run()
+		{
+			try
+			{
+				while (true)
+				{
+					readMsg();
+				}
+			}
+			catch(IOException e)
+			{
+				printMsg("Server disconnected");
+			}
+		}
+
 	}
 
 
@@ -113,3 +211,4 @@ public class ChatClient extends ChatWindow {
 	}
 
 }
+
